c50b2289d576a1d9e00c466c8c5367560be72cb9
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-31 18:55:09 -0300

    feat: exibe nome do usuário logado
    apresenta bem vindo mais nome, a partir dos dados salvos no localstorage

--- src/views/Dashboard/DashboardPage.vue
+++ src/views/Dashboard/DashboardPage.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>Dashboard Page</div>
+  <h1>Bem-vindo, {{ userName }}!</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: ''
+    }
+  },
+  mounted() {
+    this.getUserName()
+  },
+  methods: {
+    getUserName() {
+      const loggedUserData = JSON.parse(localStorage.getItem('logged_user'))
+      this.userName = loggedUserData.name
+    }
+  }
+}
+</script>
