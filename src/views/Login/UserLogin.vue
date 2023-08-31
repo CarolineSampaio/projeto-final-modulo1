b002bd51ebd4a1e18dcba94dcf25983d17bf3303
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-31 17:17:15 -0300

    feat: adiciona validação do formulário de login

--- src/views/Login/UserLogin.vue
+++ src/views/Login/UserLogin.vue
@@ -1,8 +1,18 @@
 <template>
-  <v-form>
-    <v-text-field v-model="email" label="E-mail" type="email"></v-text-field>
-    <v-text-field v-model="password" label="Senha" type="password"></v-text-field>
-    <v-btn>Logar</v-btn>
+  <v-form @submit.prevent="handleLogin">
+    <v-text-field
+      v-model="email"
+      label="E-mail"
+      type="email"
+      :error-messages="errors.email"
+    ></v-text-field>
+    <v-text-field
+      v-model="password"
+      label="Senha"
+      type="password"
+      :error-messages="errors.password"
+    ></v-text-field>
+    <v-btn type="submit">Logar</v-btn>
   </v-form>
 
   <div>
@@ -11,11 +21,42 @@
 </template>
 
 <script>
+import * as yup from 'yup'
+import { captureErrorYup } from '../../utils/captureErrorYup'
+
 export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      errors: {}
+    }
+  },
+  methods: {
+    handleLogin() {
+      const schema = yup.object().shape({
+        email: yup
+          .string()
+          .email('Forneça um endereço de email válido')
+          .required('E-mail é obrigatório.'),
+        password: yup.string().required('Senha é obrigatória.')
+      })
+      try {
+        schema.validateSync(
+          {
+            email: this.email,
+            password: this.password
+          },
+          { abortEarly: false }
+        )
+
+        this.errors = {}
+      } catch (error) {
+        if (error instanceof yup.ValidationError) {
+          console.log(error)
+          this.errors = captureErrorYup(error)
+        }
+      }
     }
   }
 }
