6fb1980437783d0b4f67d29746341e9130dac5ff
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-31 16:59:05 -0300

    feat: adiciona estrutura formulário login

--- src/views/Login/UserLogin.vue
+++ src/views/Login/UserLogin.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>User Login</div>
+  <v-form>
+    <v-text-field v-model="email" label="E-mail" type="email"></v-text-field>
+    <v-text-field v-model="password" label="Senha" type="password"></v-text-field>
+    <v-btn>Logar</v-btn>
+  </v-form>
+
+  <div>
+    <p>Ainda não tem conta?<router-link to="/signup"> Cadastre-se</router-link></p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
