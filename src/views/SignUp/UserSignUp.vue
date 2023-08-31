6e2f958aeb76a2d8720ee942c0034be884315a37
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-31 13:11:30 -0300

    feat: adiciona estrutura formulário cadastro

--- src/views/SignUp/UserSignUp.vue
+++ src/views/SignUp/UserSignUp.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>User SignUp</div>
+  <v-form>
+    <v-text-field v-model="name" label="Nome completo"></v-text-field>
+    <v-text-field v-model="email" label="E-mail"></v-text-field>
+    <v-text-field v-model="password" label="Senha" type="password"></v-text-field>
+    <v-text-field v-model="confirmPassword" label="Confirme a senha" type="password"></v-text-field>
+    <v-select model-value="bronze" label="Plano" :items="typePlan"></v-select>
+    <v-btn>Cadastrar</v-btn>
+  </v-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      typePlan: [
+        { value: 'bronze', title: 'Bronze' },
+        { value: 'prata', title: 'Prata' },
+        { value: 'ouro', title: 'Ouro' }
+      ]
+    }
+  }
+}
+</script>
