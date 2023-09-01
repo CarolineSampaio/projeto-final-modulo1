45c6bcd004a8d968bfc3188b9fc72985dff9f360
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-31 22:14:26 -0300

    feat: adiciona função pegar lista exercícios

--- src/views/Exercises/ExercisesPage.vue
+++ src/views/Exercises/ExercisesPage.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>ExercisesPage</div>
+  {{ exercises }}
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      exercises: []
+    }
+  },
+  mounted() {
+    this.getExercises()
+  },
+  methods: {
+    getExercises() {
+      axios
+        .get('http://localhost:3000/exercises')
+        .then((response) => {
+          console.log(response.data)
+          this.exercises = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+          alert('Erro ao carregar exercícios!')
+        })
+    }
+  }
+}
+</script>
