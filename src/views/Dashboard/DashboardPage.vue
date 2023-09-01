215f0579d11299fa2444a94cf72e8fa16c7ab0e2
Caroline Sampaio <caroline_08022@hotmail.com>
2023-08-31 21:00:29 -0300

    feat: adiciona integração com a api no dashboard
    obtém informações de exercicios e alunos

--- src/views/Dashboard/DashboardPage.vue
+++ src/views/Dashboard/DashboardPage.vue
@@ -3,19 +3,36 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
-      userName: ''
+      userName: '',
+      amountExercises: 0,
+      amountStudents: 0
     }
   },
   mounted() {
-    this.getUserName()
+    this.getUserName(), this.getInfo()
   },
   methods: {
     getUserName() {
       const loggedUserData = JSON.parse(localStorage.getItem('logged_user'))
       this.userName = loggedUserData.name
+    },
+    getInfo() {
+      axios
+        .get('http://localhost:3000/dashboard')
+        .then((response) => {
+          console.log(response.data)
+          this.amountExercises = response.data.amount_exercises
+          this.amountStudents = response.data.amount_students
+        })
+        .catch((error) => {
+          console.log(error)
+          alert('Erro ao carregar informações do dashboard!')
+        })
     }
   }
 }
