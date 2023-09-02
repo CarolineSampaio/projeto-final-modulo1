99fe05cb6a351758a3709e1ba3400f98cd3016ce
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-01 23:52:58 -0300

    feat: adiciona validação do formulário cadastro aluno
    validação data de nascimento criada com o uso de new date e showPicker.
    
    https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/showPicker

--- src/views/Students/NewStudent.vue
+++ src/views/Students/NewStudent.vue
@@ -1,25 +1,97 @@
 <template>
   <h1><v-icon>mdi-account-multiple</v-icon>Alunos</h1>
 
-  <v-form>
-    <v-text-field v-model="name" label="Nome completo" type="text"></v-text-field>
-    <v-text-field v-model="email" label="E-mail" type="email"></v-text-field>
-    <v-text-field v-model="contact" label="Contato" type="text"></v-text-field>
-    <v-text-field v-model="dateBirth" label="Data de Nascimento" type="date"></v-text-field>
+  <v-form @submit.prevent="CreateNewStudent">
+    <v-text-field
+      v-model="name"
+      label="Nome completo"
+      type="text"
+      :error-messages="errors.name"
+    ></v-text-field>
 
-    <v-text-field v-model="cep" label="CEP" type="text"></v-text-field>
-    <v-text-field v-model="street" label="Logradouro" type="text"></v-text-field>
-    <v-text-field v-model="number" label="Número" type="text"></v-text-field>
-    <v-text-field v-model="neighborhood" label="Bairro" type="text"></v-text-field>
-    <v-text-field v-model="city" label="Cidade" type="text"></v-text-field>
-    <v-text-field v-model="state" label="Estado" type="text"></v-text-field>
-    <v-text-field v-model="complement" label="Complemento" type="text"></v-text-field>
+    <v-text-field
+      v-model="email"
+      label="E-mail"
+      type="email"
+      :error-messages="errors.email"
+    ></v-text-field>
 
-    <v-btn>Cadastrar</v-btn>
+    <v-text-field
+      v-model="contact"
+      label="Contato"
+      type="text"
+      :error-messages="errors.contact"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="dateBirth"
+      label="Data de Nascimento"
+      type="date"
+      :error-messages="errors.dateBirth"
+      onclick="this.showPicker()"
+      onfocus="this.showPicker()"
+      onkeydown="return false"
+      :max="new Date().toLocaleDateString('pt-br').split('/').reverse().join('-')"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="cep"
+      label="CEP"
+      type="text"
+      :error-messages="errors.cep"
+      class=""
+    ></v-text-field>
+
+    <v-text-field
+      v-model="street"
+      label="Logradouro"
+      type="text"
+      :error-messages="errors.street"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="number"
+      label="Número"
+      type="text"
+      :error-messages="errors.number"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="neighborhood"
+      label="Bairro"
+      type="text"
+      :error-messages="errors.neighborhood"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="city"
+      label="Cidade"
+      type="text"
+      :error-messages="errors.city"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="state"
+      label="Estado"
+      type="text"
+      :error-messages="errors.state"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="complement"
+      label="Complemento"
+      type="text"
+      :error-messages="errors.complement"
+    ></v-text-field>
+
+    <v-btn type="">Cadastrar</v-btn>
   </v-form>
 </template>
 
 <script>
+import * as yup from 'yup'
+import { captureErrorYup } from '../../utils/captureErrorYup'
+
 export default {
   data() {
     return {
@@ -27,13 +99,57 @@ export default {
       email: '',
       contact: '',
       dateBirth: '',
+      formatedDateBirth: '',
       cep: '',
       street: '',
       number: '',
       neighborhood: '',
       city: '',
       state: '',
-      complement: ''
+      complement: '',
+
+      errors: {}
+    }
+  },
+  methods: {
+    CreateNewStudent() {
+      const schema = yup.object().shape({
+        name: yup.string().required('O nome é obrigatório.'),
+        email: yup.string().email('Forneça um e-mail válido.'),
+        contact: yup.string().required('Um telefone para contato é obrigatório.'),
+        cep: yup.string().required('O CEP é obrigatório.'),
+        street: yup.string().required('O logradouro é obrigatório.'),
+        number: yup.string().required('O número é obrigatório.'),
+        neighborhood: yup.string().required('O bairro é obrigatório.'),
+        city: yup.string().required('A cidade é obrigatória.'),
+        state: yup.string().required('O estado é obrigatório.'),
+        complement: yup.string().required('O complemento é obrigatório.')
+      })
+      this.errors = {}
+      try {
+        schema.validateSync(
+          {
+            name: this.name,
+            email: this.email,
+            contact: this.contact,
+            cep: this.cep,
+            street: this.street,
+            number: this.number,
+            neighborhood: this.neighborhood,
+            city: this.city,
+            state: this.state,
+            complement: this.complement
+          },
+          { abortEarly: false }
+        )
+        this.formatedDateBirth = this.dateBirth.split('-').reverse().join('/')
+        this.errors = {}
+      } catch (error) {
+        if (error instanceof yup.ValidationError) {
+          console.log(error)
+          this.errors = captureErrorYup(error)
+        }
+      }
     }
   }
 }
