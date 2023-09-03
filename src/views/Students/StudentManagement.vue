bc10f373bb3480387654fe2b15378c2e373b4d9e
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-03 17:37:29 -0300

    feat: adiciona estrutura básica gerenciamento alunos

--- src/views/Students/StudentManagement.vue
+++ src/views/Students/StudentManagement.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>Student Management</div>
+  <h1><v-icon>mdi-account-multiple</v-icon>Alunos</h1>
+  <router-link to="/students/new"><v-btn>novo</v-btn></router-link>
+
+  <v-form class="d-flex">
+    <v-text-field v-model="search" label="Digite o nome do aluno" type="text"></v-text-field>
+    <v-btn>Buscar</v-btn>
+  </v-form>
+
+  <v-table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Nome aluno</td>
+        <td>
+          <router-link to="/training/new"><v-btn>Montar treino</v-btn></router-link>
+          <router-link to="/training/:id"><v-btn>Ver treino</v-btn></router-link>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: ''
+    }
+  }
+}
+</script>
