656ff4565e8141866fee1961ed13cb8f060436bc
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-03 22:22:48 -0300

    feat: adiciona função obter lista de exercícios

--- src/views/Training/NewTraining.vue
+++ src/views/Training/NewTraining.vue
@@ -2,7 +2,13 @@
   <h1><v-icon>mdi-account-multiple</v-icon>Treino</h1>
 
   <v-form>
-    <v-select v-model="selectedExercise" label="Exercício"></v-select>
+    <v-select
+      v-model="selectedExercise"
+      label="Exercício"
+      :items="exercises"
+      item-title="description"
+      item-value="id"
+    ></v-select>
     <v-text-field v-model="repetitions" label="Repetições" type="number"></v-text-field>
     <v-text-field v-model="weight" label="Peso (kg)" type="number"></v-text-field>
     <v-text-field v-model="breakTime" label="Pausa" type="time"></v-text-field>
@@ -14,15 +20,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
+      exercises: [],
       selectedExercise: '',
       repetitions: '',
       weight: '',
       breakTime: '',
       weekDay: '',
       comments: ''
+    }
+  },
+  mounted() {
+    this.getExercises()
+  },
+  methods: {
+    getExercises() {
+      axios.get('http://localhost:3000/exercises').then(({ data }) => {
+        this.exercises = data
+      })
     }
   }
 }
