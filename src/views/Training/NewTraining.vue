176f5b0b4a2812dee22a2d885e091178906c7149
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-04 00:00:15 -0300

    feat: adiciona regras de validação novo treino

--- src/views/Training/NewTraining.vue
+++ src/views/Training/NewTraining.vue
@@ -1,19 +1,48 @@
 <template>
   <h1><v-icon>mdi-account-multiple</v-icon>Treino</h1>
 
-  <v-form>
+  <v-form @submit.prevent="createTraining">
     <v-select
       v-model="selectedExercise"
       label="Exercício"
       :items="exercises"
       item-title="description"
       item-value="id"
+      :error-messages="errors.selectedExercise"
     ></v-select>
-    <v-text-field v-model="repetitions" label="Repetições" type="number"></v-text-field>
-    <v-text-field v-model="weight" label="Peso (kg)" type="number"></v-text-field>
-    <v-text-field v-model="breakTime" label="Pausa" type="time"></v-text-field>
-    <v-select v-model="weekDay" label="Dia da semana"></v-select>
+
+    <v-text-field
+      v-model="repetitions"
+      label="Repetições"
+      type="number"
+      :error-messages="errors.repetitions"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="weight"
+      label="Peso (kg)"
+      type="number"
+      :error-messages="errors.weight"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="breakTime"
+      label="Pausa"
+      type="time"
+      min="00:00:10"
+      max="00:10:00"
+      :rules="breakTimeRules"
+    ></v-text-field>
+
+    <v-select
+      v-model="weekDay"
+      label="Dia da semana"
+      :items="weekList"
+      :error-messages="errors.weekDay"
+    ></v-select>
+
     <v-textarea v-model="comments" label="Observações"></v-textarea>
+
     <router-link to="/students"><v-btn>Cancelar</v-btn></router-link>
     <v-btn type="submit">Cadastrar</v-btn>
   </v-form>
@@ -21,26 +50,78 @@
 
 <script>
 import axios from 'axios'
+import * as yup from 'yup'
+import { captureErrorYup } from '../../utils/captureErrorYup'
 export default {
   data() {
     return {
       exercises: [],
       selectedExercise: '',
-      repetitions: '',
-      weight: '',
+      repetitions: null,
+      weight: null,
       breakTime: '',
       weekDay: '',
-      comments: ''
+      weekList: [
+        { value: 'domingo', title: 'Domingo' },
+        { value: 'segunda', title: 'Segunda-Feira' },
+        { value: 'terca', title: 'Terça-Feira' },
+        { value: 'quarta', title: 'Quarta-Feira' },
+        { value: 'quinta', title: 'Quinta-Feira' },
+        { value: 'sexta', title: 'Sexta-Feira' },
+        { value: 'sabado', title: 'Sábado' }
+      ],
+      comments: '',
+      errors: []
     }
   },
   mounted() {
     this.getExercises()
+    this.weekDay = this.currentWeekDay()
   },
   methods: {
     getExercises() {
       axios.get('http://localhost:3000/exercises').then(({ data }) => {
         this.exercises = data
       })
+    },
+    createTraining() {
+      const schema = yup.object().shape({
+        selectedExercise: yup.string().required('Selecione um exercício da lista.'),
+        repetitions: yup.number().required('Insira no mínimo uma repetição.'),
+        weight: yup.number().required('Forneça o peso necessário para a execução do exercício.'),
+        weekDay: yup.string().required('Selecione um dia da semana.')
+      })
+
+      try {
+        schema.validateSync(
+          {
+            selectedExercise: this.selectedExercise,
+            repetitions: this.repetitions,
+            weight: this.weight,
+            weekDay: this.weekDay
+          },
+          { abortEarly: false }
+        )
+        this.errors = {}
+      } catch (error) {
+        if (error instanceof yup.ValidationError) {
+          console.log(error)
+          this.errors = captureErrorYup(error)
+        }
+      }
+    },
+    currentWeekDay() {
+      const weekDay = new Date().getDay()
+      return this.weekList[weekDay].value
+    }
+  },
+  computed: {
+    breakTimeRules() {
+      return [
+        (v) => !!v || 'Forneça o tempo de pausa necessário entre as repetições do exercício.',
+        (v) => v >= '00:00:10' || 'A pausa mínima é de 10 segundos',
+        (v) => v <= '00:10:00' || 'A pausa máxima é de 10 minutos'
+      ]
     }
   }
 }
