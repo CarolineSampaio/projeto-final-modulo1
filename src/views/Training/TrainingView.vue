c36fdd21b2fde4b3707bcb564bcdc096fade0547
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-05 20:45:50 -0300

    feat: adiciona requisição para buscar treino aluno

--- src/views/Training/TrainingView.vue
+++ src/views/Training/TrainingView.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>Training View</div>
+  <h1><v-icon>mdi-account-multiple</v-icon>Treinos</h1>
+
+  <h2>Hoje</h2>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      studentId: this.$route.params.id,
+      workoutTable: []
+    }
+  },
+  mounted() {
+    this.getTraining()
+  },
+  methods: {
+    getTraining() {
+      axios
+        .get(`http://localhost:3000/workouts?student_id=${this.studentId}`)
+        .then(({ data }) => {
+          this.workoutTable = data
+          console.log(this.workoutTable)
+        })
+        .catch((error) => {
+          console.log(error)
+          alert('Houve um erro ao carregar os treinos')
+        })
+    }
+  }
+}
+</script>
