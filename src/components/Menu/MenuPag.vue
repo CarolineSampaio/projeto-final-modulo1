1be4f3cc9e388e404b7748b087da6eedb064d3df
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-08 18:17:15 -0300

    feat: adiciona componente de menu inicial
    menu aparece conforme a pagina, função remover user do local storage.

---
+++ src/components/Menu/MenuPag.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <h1>Menu</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/students">Alunos</router-link>
+    <router-link to="/exercises">Exercícios</router-link>
+    <router-link to="/login" @click="logout">Sair</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MenuPag',
+
+  methods: {
+    logout() {
+      localStorage.removeItem('logged_user')
+    }
+  }
+}
+</script>
