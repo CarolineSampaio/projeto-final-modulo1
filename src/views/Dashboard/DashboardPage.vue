ec7c1a4265a33c4608ceda52c00fc3c72a8f04ea
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-13 01:01:51 -0300

    style: adiciona layout base dashboard

--- src/views/Dashboard/DashboardPage.vue
+++ src/views/Dashboard/DashboardPage.vue
@@ -1,29 +1,46 @@
 <template>
-  <h1>Bem-vindo, {{ userName }}!</h1>
-  <div class="d-flex">
-    <v-card variant="outlined" width="300px">
-      <v-row class="ma-2 align-center">
-        <v-col cols="9">
-          <v-card-title>{{ amountStudents }} Alunos</v-card-title>
-          <router-link to="/students/new"><v-btn>Adicionar</v-btn></router-link>
-        </v-col>
-        <v-col cols="3">
-          <v-icon size="xx-large">mdi-account</v-icon>
-        </v-col>
-      </v-row>
-    </v-card>
+  <div class="container">
+    <h1 class="my-10 font-weight-medium">Dashboard</h1>
 
-    <v-card variant="outlined" width="300px">
-      <v-row class="ma-2 align-center">
-        <v-col cols="9">
-          <v-card-title>{{ amountExercises }} Exercícios</v-card-title>
-          <router-link to="/exercises"><v-btn>Adicionar</v-btn></router-link>
-        </v-col>
-        <v-col cols="3">
-          <v-icon size="xx-large">mdi-arm-flex</v-icon>
-        </v-col>
-      </v-row>
-    </v-card>
+    <div class="d-flex justify-space-between align-center mt-16 mcards">
+      <v-card width="400px" height="700px" class="welcomeMessage">
+        <v-row class="ma-2 align-center">
+          <v-col>
+            <h2 class="font-weight-bold">Olá, {{ userName }}!</h2>
+            <h4 class="mt-3 font-weight-medium">
+              O sucesso de seus alunos começa com você. Vamos começar!
+            </h4>
+          </v-col>
+          <v-col cols="9">
+            <img src="../../assets/woman_pilates.png" alt="Welcome" height="300" />
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <v-card width="400px" height="700px" class="welcomeMessage">
+        <v-row class="ma-2 align-center">
+          <v-col>
+            <img src="../../assets/woman_pilates.png" alt="Welcome" height="300" />
+          </v-col>
+          <v-col cols="9">
+            <v-card-title>{{ amountStudents }} Alunos</v-card-title>
+            <router-link to="/students/new"><v-btn>Adicionar</v-btn></router-link>
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <v-card width="400px" height="700px" class="welcomeMessage">
+        <v-row class="ma-2 align-center">
+          <v-col>
+            <img src="../../assets/woman_pilates.png" alt="Welcome" height="300" />
+          </v-col>
+          <v-col cols="9">
+            <v-card-title>{{ amountExercises }} Exercícios</v-card-title>
+            <router-link to="/exercises"><v-btn>Adicionar</v-btn></router-link>
+          </v-col>
+        </v-row>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -68,3 +85,20 @@ export default {
   }
 }
 </script>
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  padding-left: 20%;
+  padding-right: 5%;
+}
+.welcomeMessage {
+  background-color: #ffca28;
+  border-radius: 2rem;
+  width: 100%;
+}
+.cards {
+  width: 100%;
+}
+</style>
+s
