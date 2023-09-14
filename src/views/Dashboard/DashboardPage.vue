e3848e1ff8eee205627436c33a9c9450b2397799
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-14 00:38:29 -0300

    style: implementa responsividade pagina dashboard

--- src/views/Dashboard/DashboardPage.vue
+++ src/views/Dashboard/DashboardPage.vue
@@ -1,76 +1,108 @@
 <template>
-  <div class="container">
-    <h1 class="py-12 font-weight-medium">Dashboard</h1>
+  <div class="container" :style="mdAndDown ? 'padding-left: 5%' : ''">
+    <h1 class="py-4 py-md-12 font-weight-medium" :style="smAndDown ? 'text-align:center;' : ''">
+      Dashboard
+    </h1>
 
-    <v-card class="welcomeMessage">
+    <v-card class="welcomeMessage d-flex align-flex-end">
       <v-row class="ma-2">
-        <v-col class="ml-7 my-14">
+        <v-col class="ml-2 ml-md-7 my-4 my-md-6 my-lg-14">
           <h2 class="font-weight-bold">Olá, {{ userName }}!</h2>
           <h3 class="mt-3 font-weight-medium">
             O sucesso de seus alunos começa com você. Vamos começar!
           </h3>
         </v-col>
-        <v-col cols="6" class="d-flex justify-end">
+        <v-col cols="0" md="6" class="d-flex justify-end pr-0">
           <img
             src="../../assets/woman_pilates.svg"
             alt="Mulher fazendo pilates."
-            class="welcomeImage"
+            class="welcomeImage hidden-sm-and-down"
+            :style="
+              smAndDown
+                ? 'height: 25vh; transform: translateY(-20%);'
+                : 'height: 35vh; transform: translateY(-42%);'
+            "
           />
         </v-col>
       </v-row>
     </v-card>
 
-    <div class="d-flex justify-space-between align-center mt-16 teste">
-      <v-card class="managementCards">
+    <div
+      class="d-flex justify-space-between align-center mt-5 mt-md-16"
+      :style="smAndDown ? 'flex-direction: column;' : 'flex-direction: row'"
+    >
+      <v-card
+        class="managementCards"
+        :style="
+          smAndDown
+            ? 'width: 100%; margin-bottom: 5%'
+            : 'width: 48%; height: 50%; margin-bottom: 2%'
+        "
+      >
         <v-row class="ma-2">
-          <v-col>
+          <v-col cols="12" lg="6" class="d-flex align-center justify-center pa-0">
             <img
               src="../../assets/students.svg"
               alt="imagem vetorizada de três mulheres com roupa de academia."
               class="imageCards"
             />
           </v-col>
-          <v-col class="d-flex flex-column justify-space-evenly align-center">
-            <v-card-title class="text-h5 font-weight-bold">
+          <v-col cols="12" lg="6" class="d-flex flex-column justify-space-evenly align-center pa-0">
+            <v-card-title
+              class="text-h6 text-md-h5 font-weight-bold"
+              :style="lgAndDown ? 'white-space: break-spaces; text-align: center' : ''"
+            >
               <span class="text-h4 font-weight-bold">{{ amountStudents }}</span>
               Alunos Cadastrados
             </v-card-title>
             <router-link to="/students/new">
               <v-btn
                 size="large"
-                variant="flat"
-                color="grey-darken-3 text-amber"
+                variant="elevated"
+                color="grey-darken-4 text-amber"
                 class="font-weight-bold"
+                :ripple="false"
               >
-                Adicionar Aluno
+                Adicionar
               </v-btn>
             </router-link>
           </v-col>
         </v-row>
       </v-card>
 
-      <v-card class="managementCards">
+      <v-card
+        class="managementCards"
+        :style="
+          smAndDown
+            ? 'width: 100%; margin-bottom: 5%'
+            : 'width: 48%; height: 50%; margin-bottom: 2%'
+        "
+      >
         <v-row class="ma-2">
-          <v-col>
+          <v-col cols="12" lg="6" class="d-flex align-center justify-center pa-0">
             <img
               src="../../assets/exercises.svg"
               alt="imagem vetorizada de três mulheres com roupa de academia."
               class="imageCards"
             />
           </v-col>
-          <v-col class="d-flex flex-column justify-space-evenly align-center">
-            <v-card-title class="text-h5 font-weight-bold">
+          <v-col cols="12" lg="6" class="d-flex flex-column justify-space-evenly align-center pa-0">
+            <v-card-title
+              class="text-h6 text-md-h5 font-weight-bold"
+              :style="lgAndDown ? 'white-space: break-spaces; text-align: center' : ''"
+            >
               <span class="text-h4 font-weight-bold">{{ amountExercises }}</span>
               Exercícios Cadastrados
             </v-card-title>
             <router-link to="/exercises">
               <v-btn
                 size="large"
-                variant="flat"
-                color="grey-darken-3 text-amber"
+                variant="elevated"
+                color="grey-darken-4 text-amber"
                 class="font-weight-bold"
+                :ripple="false"
               >
-                Adicionar Exercício
+                Adicionar
               </v-btn>
             </router-link>
           </v-col>
@@ -79,6 +111,11 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+const { smAndDown, mdAndDown, lgAndDown } = useDisplay()
+</script>
 
 <script>
 import axios from 'axios'
@@ -128,24 +165,28 @@ export default {
   padding-left: 20%;
   padding-right: 5%;
 }
+.welcomeMessage,
+.managementCards {
+  border-radius: 2rem;
+  background: rgb(255, 212, 80);
+  background: linear-gradient(160deg, rgba(255, 212, 80, 1) 0%, rgba(222, 167, 0, 1) 100%);
+  box-shadow:
+    12px 16px 28px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
+    0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
+    0px 2px 4px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12)),
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8));
+}
 .welcomeMessage {
   width: 100%;
   height: 22%;
-  background-color: #ffca28;
-  border-radius: 2rem;
+
   overflow: initial;
 }
-.welcomeImage {
-  height: 35vh;
-  transform: translateY(-42%);
-}
-.managementCards {
-  width: 48%;
-  height: 50vh;
-  background-color: #ffca28;
-  border-radius: 2rem;
-}
 .imageCards {
-  height: 45vh;
+  height: 90%;
+  object-fit: cover;
+}
+.v-btn:hover {
+  background-color: #303030 !important;
 }
 </style>
