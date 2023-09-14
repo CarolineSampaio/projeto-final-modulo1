415d883eb43a9ce73472f257d94cf22b1a1068d0
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-14 15:42:28 -0300

    style: implementa design responsivo pag exercicios

--- src/views/Exercises/ExercisesPage.vue
+++ src/views/Exercises/ExercisesPage.vue
@@ -1,38 +1,65 @@
 <template>
-  <v-container class="ma-0 pa-5">
-    <v-row class="align-center">
-      <v-icon size="xx-large">mdi-arm-flex</v-icon>
-      <h1>Exercícios</h1>
-    </v-row>
+  <div class="container" :style="mdAndDown ? 'padding-left: 5%' : 'padding-left: 20%'">
+    <div class="d-flex align-center" :style="smAndDown ? 'justify-content:center;' : ''">
+      <h1 class="py-4 py-md-12 font-weight-medium">Exercícios</h1>
+      <v-icon size="x-large" class="pl-10" color="amber">mdi-weight-lifter</v-icon>
+    </div>
 
-    <v-form @submit.prevent="addExercise">
-      <v-text-field
-        v-model="description"
-        label="Nome do exercício"
-        class="mt-5"
-        :error-messages="errors.description"
-      ></v-text-field>
-      <v-btn type="submit">Cadastrar</v-btn>
+    <div class="cardImage">
+      <div
+        class="cardContent"
+        :style="smAndDown ? 'flex-direction: column;  padding:8%' : 'flex-direction: row'"
+      >
+        <v-form
+          @submit.prevent="addExercise"
+          class="d-flex"
+          :style="xs ? 'flex-direction: column;' : 'flex-direction: row'"
+        >
+          <v-text-field
+            v-model="description"
+            label="Digite o nome do exercício"
+            :error-messages="errors.description"
+            variant="outlined"
+            class="pl-md-2"
+          >
+          </v-text-field>
+          <v-btn
+            type="submit"
+            variant="elevated"
+            color="grey-darken-4 text-amber"
+            class="font-weight-bold px-md-16 ml-sm-5 ml-md-10 mt-2 mt-md-0"
+            height="60px"
+            :ripple="false"
+            :style="xs ? 'height: 45px;' : 'height: 60px'"
+            >Cadastrar</v-btn
+          >
+        </v-form>
 
-      <v-snackbar v-model="snackbar" :timeout="duration" color="success" location="top">
-        Cadastrado com sucesso!
-      </v-snackbar>
-    </v-form>
+        <v-table class="mt-4 mt-md-10">
+          <thead>
+            <tr>
+              <th class="font-weight-bold text-grey-darken-4">Nome do exercício</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="exercise in exercises" :key="exercise.id">
+              <td>{{ exercise.description }}</td>
+            </tr>
+          </tbody>
+        </v-table>
 
-    <v-table>
-      <thead>
-        <tr>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="exercise in exercises" :key="exercise.id">
-          <td>{{ exercise.description }}</td>
-        </tr>
-      </tbody>
-    </v-table>
-  </v-container>
+        <v-snackbar v-model="snackbar" :timeout="duration" color="success" location="top">
+          Cadastrado com sucesso!
+        </v-snackbar>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+const { xs, smAndDown, mdAndDown } = useDisplay()
+</script>
 
 <script>
 import axios from 'axios'
@@ -118,3 +145,38 @@ export default {
   }
 }
 </script>
+
+<style>
+.container {
+  width: 100%;
+  min-height: 100%;
+  padding-left: 20%;
+  padding-right: 5%;
+  padding-bottom: 2%;
+}
+.cardImage {
+  border-radius: 2rem;
+  background-image: url(../../assets/bg_pags.jpg);
+  background-size: cover;
+  box-shadow:
+    12px 16px 28px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
+    0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
+    0px 2px 4px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12)),
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8));
+  padding: 4%;
+}
+.cardContent {
+  padding: 4%;
+  background-color: rgba(255, 255, 255, 0.3);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border-radius: 2rem;
+  box-shadow:
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8)),
+    1px 1px 0px var(--v-shadow-key-penumbra-opacity, rgba(230, 230, 230, 0.8));
+}
+
+.v-table {
+  background-color: transparent !important;
+}
+</style>
