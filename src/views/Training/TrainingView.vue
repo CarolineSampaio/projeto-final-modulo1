48f2dc2815001a47a6770d3e764e9b148e60076b
Caroline Sampaio <caroline_08022@hotmail.com>
2023-09-15 18:28:07 -0300

    style: implementa design responsivo pag ver treino
    atualiza main css com estilos da pagina que precisam ser globais

--- src/views/Training/TrainingView.vue
+++ src/views/Training/TrainingView.vue
@@ -1,46 +1,91 @@
 <template>
-  <h1><v-icon>mdi-account-multiple</v-icon>Treinos - {{ studentName }}</h1>
+  <div class="container" :style="mdAndDown ? 'padding-left: 5%' : 'padding-left: 20%'">
+    <div class="d-flex align-center" :style="smAndDown ? 'justify-content:center;' : ''">
+      <h1 class="py-4 py-md-12 font-weight-medium" :style="smAndDown ? 'font-size:1.5rem' : ''">
+        Treinos - {{ this.studentName }}
+      </h1>
+      <v-icon size="x-large" class="pl-10" color="amber">mdi-account-outline</v-icon>
+    </div>
 
-  <h2>Hoje</h2>
+    <div class="cardImage">
+      <div class="cardContent" :style="smAndDown ? 'flex-direction: column;  padding:8%' : ''">
+        <h2 class="pb-2 pb-sm-5 font-weight-medium">Hoje</h2>
+        <div v-for="workout in workoutTable[today]" :key="workout">
+          <v-checkbox
+            :label="`${workout.exercise_description} | ${workout.weight}KG | ${workout.repetitions} repetições | ${workout.break_time} segundos de pausa`"
+            v-model="workout.checked"
+            v-bind:true-value="1"
+            @click="markAsChecked(workout.id)"
+            hide-details
+            :style="smAndDown ? 'padding-top: 5%;' : ''"
+          ></v-checkbox>
+        </div>
 
-  <div v-for="workout in workoutTable[2]" :key="workout">
-    <v-checkbox
-      :label="`${workout.exercise_description} | ${workout.weight}KG | ${workout.repetitions} repetições | ${workout.break_time} segundos de pausa`"
-      v-model="workout.checked"
-      v-bind:true-value="1"
-      @click="markAsChecked(workout.id)"
-      hide-details
-    ></v-checkbox>
+        <v-card class="my-12 pb-5">
+          <v-tabs v-model="activeTab" align-tabs="center">
+            <v-tab :value="1">Segunda</v-tab>
+            <v-tab :value="2">Terça</v-tab>
+            <v-tab :value="3">Quarta</v-tab>
+            <v-tab :value="4">Quinta</v-tab>
+            <v-tab :value="5">Sexta</v-tab>
+            <v-tab :value="6">Sábado</v-tab>
+            <v-tab :value="0">Domingo</v-tab>
+          </v-tabs>
+          <v-window v-model="activeTab">
+            <v-window-item v-for="workoutDay in workoutTable" :key="workoutDay">
+              <v-container fluid>
+                <v-table>
+                  <tbody>
+                    <tr v-for="workout in workoutDay" :key="workout" class="d-none d-sm-table-row">
+                      <td class="pa-sm-2">{{ workout.exercise_description }}</td>
+                      <td class="pa-sm-2">{{ workout.weight }}KG</td>
+                      <td class="pa-sm-2">{{ workout.repetitions }} repetições</td>
+                      <td class="pa-sm-2">{{ workout.break_time }} segundos de pausa</td>
+                    </tr>
+
+                    <tr v-for="workout in workoutDay" :key="workout" class="d-sm-none">
+                      <td class="pa-1 ma-1 mb-2 text-center">
+                        <div class="mb-2 text-body-2">{{ workout.exercise_description }}</div>
+                        <div class="mb-2 text-body-2">{{ workout.weight }} KG</div>
+                      </td>
+                      <td class="pa-1 ma-1 mb-2 text-center">
+                        <div class="mb-2 text-body-2">{{ workout.repetitions }} repetições</div>
+                        <div class="mb-2 text-body-2">{{ workout.break_time }} de pausa</div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </v-table>
+              </v-container>
+            </v-window-item>
+          </v-window>
+        </v-card>
+
+        <div>
+          <v-row>
+            <v-col cols="12" class="d-flex justify-center">
+              <router-link to="/students">
+                <v-btn
+                  variant="elevated"
+                  size="large"
+                  color="amber text-dark-grey-4"
+                  class="font-weight-bold px-sm-2 px-md-10 mr-1 mr-md-4"
+                  :ripple="false"
+                >
+                  Voltar
+                </v-btn>
+              </router-link>
+            </v-col>
+          </v-row>
+        </div>
+      </div>
+    </div>
   </div>
-
-  <v-card>
-    <v-tabs v-model="activeTab" align-tabs="center">
-      <v-tab :value="1">Segunda</v-tab>
-      <v-tab :value="2">Terça</v-tab>
-      <v-tab :value="3">Quarta</v-tab>
-      <v-tab :value="4">Quinta</v-tab>
-      <v-tab :value="5">Sexta</v-tab>
-      <v-tab :value="6">Sábado</v-tab>
-      <v-tab :value="0">Domingo</v-tab>
-    </v-tabs>
-    <v-window v-model="activeTab">
-      <v-window-item v-for="workoutDay in workoutTable" :key="workoutDay">
-        <v-container fluid>
-          <v-table>
-            <tbody>
-              <tr v-for="workout in workoutDay" :key="workout">
-                <td>{{ workout.exercise_description }}</td>
-                <td>{{ workout.weight }}KG</td>
-                <td>{{ workout.repetitions }} repetições</td>
-                <td>{{ workout.break_time }} segundos de pausa</td>
-              </tr>
-            </tbody>
-          </v-table>
-        </v-container>
-      </v-window-item>
-    </v-window>
-  </v-card>
 </template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+const { smAndDown, mdAndDown } = useDisplay()
+</script>
 
 <script>
 import axios from 'axios'
@@ -129,3 +174,19 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.v-slide-group-item--active {
+  background-color: #ffc107 !important;
+}
+.v-card {
+  background-color: rgba(255, 255, 255, 0.5) !important;
+  box-shadow:
+    inset 1px 1px 0px 0px var(--v-shadow-key-penumbra-opacity, rgba(255, 255, 255, 0.8)),
+    1px 1px 0px var(--v-shadow-key-penumbra-opacity, rgba(225, 225, 225, 0.8));
+}
+
+td {
+  width: 25%;
+}
+</style>
